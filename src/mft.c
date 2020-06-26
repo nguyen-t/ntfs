8c254061e6a145ce2df85ff28760848f19d98b61
Tuan <tuanmng1999@gmail.com>
2020-06-25 19:01:12 -0500

    Added MFT mirror and MFT union type

--- src/mft.c
+++ src/mft.c
@@ -4,22 +4,58 @@
 #include "vbr.h"
 #include "mft.h"
 
-int load_mft(int fd, uint8_t mft[1024], VBR* bs, off_t base) {
-  off_t start;
+int load_mft(int fd, MFT* mft, VBR* vbr, off_t base) {
+  off_t current;
   int bytes_read;
-  uint16_t sector_size = *bs->bpb.bytes_per_sector;
-  uint8_t cluster_size = *bs->bpb.sectors_per_cluster;
-  uint64_t mft_cluster = *bs->ebpb.mft_cluster_number;
-  uint64_t mft_offset = sector_size * cluster_size * mft_cluster + base;
+  uint16_t sector_size = *vbr->bpb.bytes_per_sector;
+  uint8_t cluster_size = *vbr->bpb.sectors_per_cluster;
+  uint64_t mft_cluster = *vbr->ebpb.mft_cluster_number;
+  uint64_t mft_offset  = sector_size * cluster_size * mft_cluster + base;
 
-  if((start = lseek(fd, 0, SEEK_CUR)) < 0) return -1;
+  // Save current file pointer
+  if((current = lseek(fd, 0, SEEK_CUR)) < 0) {
+    return -1;
+  }
 
   // Find and read MFT
-  if(lseek(fd, mft_offset, SEEK_SET) < 0) return -1;
-  if((bytes_read = read(fd, mft, 1024)) < 0) return -1;
+  if(lseek(fd, mft_offset, SEEK_SET) < 0)  {
+    return -1;
+  }
+  if((bytes_read = read(fd, mft->raw, 1024)) < 0) {
+    return -1;
+  }
 
   // Reset file pointer
-  if(lseek(fd, start, SEEK_SET) < 0) return -1;
+  if(lseek(fd, current, SEEK_SET) < 0) return -1;
+
+  return bytes_read;
+}
+
+int load_mft_mirror(int fd, MFT* mft, VBR* vbr, off_t base) {
+  off_t current;
+  int bytes_read;
+  uint16_t sector_size        = *vbr->bpb.bytes_per_sector;
+  uint8_t cluster_size        = *vbr->bpb.sectors_per_cluster;
+  uint64_t mft_mirror_cluster = *vbr->ebpb.mft_mirror_cluster_number;
+  uint64_t mft_offset         = sector_size * cluster_size * mft_mirror_cluster + base;
+
+  // Save current file pointer
+  if((current = lseek(fd, 0, SEEK_CUR)) < 0) {
+    return -1;
+  }
+
+  // Find and read MFT mirror
+  if(lseek(fd, mft_offset, SEEK_SET) < 0) {
+    return -1;
+  }
+  if((bytes_read = read(fd, mft->raw, 1024)) < 0) {
+    return -1;
+  }
+
+  // Reset file pointer
+  if(lseek(fd, current, SEEK_SET) < 0) {
+    return -1;
+  }
 
   return bytes_read;
 }
